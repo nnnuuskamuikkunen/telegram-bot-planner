bccaedd6e756c119d3afca248b9edff40bbb6a25
schwaermer <aurora2204@yandex.ru>
2025-05-21 18:19:29 +0300

    почти финальный бот
    объединила основной код с кодом ввода даты и времени для поиска по дате и с кодом для редактирования.

--- bot.py
+++ bot.py
@@ -9,8 +9,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from database import init_db, add_note, get_user_notes, delete_note, get_notes_for_reminders
-from database import mark_reminder_sent, get_note_by_id,get_upcoming_notes, get_notes_by_type, get_notes_by_date
+from database import init_db, add_note, get_user_notes, delete_note, get_notes_for_reminders, edit_notes
+from database import mark_reminder_sent, get_note_by_id, get_notes_by_type, get_notes_by_date
+#from scheduler import book_timeslot
 from config import BOT_TOKEN, DATABASE_NAME
 
 # Включаем логирование
@@ -34,8 +35,13 @@ class AddNoteStates(StatesGroup):
     waiting_for_minute = State()
     waiting_for_date = State()
     waiting_for_type = State()
+    waiting_for_edit = State()
     type_input = State()
     hour_input = State()
+    synchronize = State()
+    
+class SearchStates(StatesGroup):
+    waiting_for_search_date = State()
 
 
 # Генерация календаря
@@ -133,8 +139,7 @@ async def command_start_handler(message: types.Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Добавить заметку", callback_data="add_note")],
         [InlineKeyboardButton(text="Мои заметки", callback_data="list_notes")],
-        [InlineKeyboardButton(text="Поиск по категории", callback_data="show_by_type")],
-        [InlineKeyboardButton(text="Поиск по дате", callback_data="show_by_date")],
+        [InlineKeyboardButton(text="Поиск заметок", callback_data="show_notes")],
         [InlineKeyboardButton(text="Помощь", callback_data="show_help")]
     ])
 
@@ -144,6 +149,13 @@ async def command_start_handler(message: types.Message):
         reply_markup=keyboard
     )
 
+@router.callback_query(F.data == "show_notes")
+async def show_notes_handler(callback: types.CallbackQuery):
+    await callback.message.edit_text("Поиск заметок:", reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Поиск по категории", callback_data="show_by_type")],
+        [InlineKeyboardButton(text="Поиск по дате", callback_data="show_by_date")],
+        [InlineKeyboardButton(text="Назад", callback_data="back_to_main")]]))
+    await callback.answer()
 
 @router.callback_query(F.data == "show_help")
 async def show_help_handler(callback: types.CallbackQuery):
@@ -167,12 +179,12 @@ async def back_to_main_handler(callback: types.CallbackQuery):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Добавить заметку", callback_data="add_note")],
         [InlineKeyboardButton(text="Мои заметки", callback_data="list_notes")],
-        [InlineKeyboardButton(text="Поиск по дате", callback_data="show_by_date")],
-        [InlineKeyboardButton(text="Поиск по категории", callback_data="show_by_type")],
+        [InlineKeyboardButton(text="Поиск заметок", callback_data="show_notes")],
         [InlineKeyboardButton(text="Помощь", callback_data="show_help")]
     ])
     await callback.message.edit_text("главное меню", reply_markup=keyboard)
     await callback.answer()
+
 
 
 @router.callback_query(F.data.startswith("list_notes"))
@@ -265,7 +277,9 @@ async def view_note_handler(callback: types.CallbackQuery):
         return
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Синхронизировать с гугл календарем", callback_data=f"synchronize_{note_id}")],
         [InlineKeyboardButton(text="Редактировать", callback_data=f"edit_{note_id}")],
+        [InlineKeyboardButton(text="Отметить как выполненное", callback_data=f"complete_{note_id}")],
         [InlineKeyboardButton(text="Удалить", callback_data=f"delete_{note_id}")],
         [InlineKeyboardButton(text="Назад к списку", callback_data="list_notes")]
     ])
@@ -278,24 +292,11 @@ async def view_note_handler(callback: types.CallbackQuery):
     await callback.answer()
 
 
-@router.callback_query(F.data == "show_by_date")
-async def ask_date_for_notes_handler(callback: types.CallbackQuery, ):
-    """Запрашивает дату для поиска заметок"""
-    await callback.message.edit_text(
-        "Введите дату в формате ГГГГ-ММ-ДД для поиска заметок:\n\n"
-        "Пример: <code>2023-12-31</code>",
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="Отмена", callback_data="back_to_main")]
-        ]),
-        parse_mode="HTML"
-    )
-    
-    await callback.answer()
 
 
-@router.message(F.text.regexp(r'^\d{4}-\d{2}-\d{2}$'))
+"""@router.message(F.text.regexp(r'^\d{4}-\d{2}-\d{2}$'))
 async def handle_date_search(message: types.Message):
-    """Обрабатывает поиск заметок по дате"""
+    Обрабатывает поиск заметок по дате
     search_date = message.text.strip()
     user_id = message.from_user.id
     notes = await get_notes_by_date(DATABASE_NAME, user_id, search_date)
@@ -325,6 +326,49 @@ async def handle_date_search(message: types.Message):
         message_text,
         reply_markup=keyboard
     )
+
+
+
+"""
+
+
+"""синхронизация"""
+@router.callback_query(F.data.startswith("synchronize_"))
+async def synchronize(callback: types.CallbackQuery, state: FSMContext):
+    """Запрашивает почту"""
+    await state.update_data(note_id=int(callback.data.split('_')[1]))
+    await callback.message.edit_text(
+        "Введите гугл почту:",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Отмена", callback_data="back_to_main")]
+        ]),
+        parse_mode="HTML"
+    )
+    await state.set_state(AddNoteStates.synchronize)
+    await callback.answer()
+
+@router.message(F.text.regexp(r'[a-z]+([\.-]*[a-z]*)*@gmail.com'), AddNoteStates.synchronize)
+async def process_note_text(message: types.Message, state: FSMContext):
+    """Обрабатывает почту и запрашивает категорию"""
+    mail = message.text.strip().lower()
+    user_data = await state.get_data()
+    note = get_note_by_id(DATABASE_NAME, user_data["note_id"], message.from_user.id)
+    date_time = f'{note["note_date"]} {note["note_date"]}'
+    #await book_timeslot(note["note_text"], date_time, mail)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Посмотреть все заметки", callback_data="list_notes")],
+            [InlineKeyboardButton(text="В главное меню", callback_data="back_to_main")]
+        ])
+
+    await message.answer(
+            f"Вам на почту придет ссылка, по которой надо перейти для синхронизации.",
+            reply_markup=keyboard,
+            parse_mode="HTML"
+        )
+
+
+
+
 
 
 
@@ -381,8 +425,68 @@ async def handle_type_search(message: types.Message, state: FSMContext):
 
 
 
+    """редактирование"""
+
+@router.callback_query(F.data.startswith("edit_"))
+async def handle_edit_button(callback: types.CallbackQuery, state: FSMContext):
+    """Обработка нажатия кнопки 'Редактировать'"""
+    await state.set_state(AddNoteStates.waiting_for_edit)
+    await state.update_data(note_id=int(callback.data.split('_')[1]))
+    await callback.message.edit_text(
+        "Введите новый текст заметки:",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Отмена", callback_data="back_to_main")]
+        ])
+    )
+    await callback.answer()
+
+@router.message(AddNoteStates.waiting_for_edit)
+async def process_edit(message: types.Message, state: FSMContext):
+    """Обрабатывает текст заметки и сохраняет изменения"""
+    await state.update_data(new_text=message.text)
+    user_data = await state.get_data()
+    await edit_notes(DATABASE_NAME, message.from_user.id, user_data["note_id"], user_data["new_text"])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Посмотреть все заметки", callback_data="list_notes")],
+            [InlineKeyboardButton(text="В главное меню", callback_data="back_to_main")]
+        ])
+
+    await message.answer(
+            f"Заметка успешно изменена!",
+            reply_markup=keyboard,
+            parse_mode="HTML"
+        )
+    await state.clear()
+    
 
 
+
+
+"""отметка выполненное"""
+
+@router.callback_query(F.data.startswith("complete_"))
+async def save_as_complete(callback: types.CallbackQuery, state: FSMContext):
+    """Обработка нажатия кнопки 'Отметить как выполненное'"""
+    
+    note_id=int(callback.data.split('_')[1])
+    note_data = await get_note_by_id(DATABASE_NAME, note_id, callback.from_user.id)
+    new_text = f"{note_data["note_text"]} ✅"
+    await edit_notes(DATABASE_NAME, callback.from_user.id, note_id, new_text)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Посмотреть все заметки", callback_data="list_notes")],
+            [InlineKeyboardButton(text="В главное меню", callback_data="back_to_main")]
+        ])
+
+    await callback.message.edit_text(
+            f"Заметка отмечена как выполненная!",
+            reply_markup=keyboard,
+            parse_mode="HTML"
+        )
+
+
+
+
+    
 """новый ввод даты и времени"""
 
 @router.callback_query(F.data == "add_note")
@@ -409,7 +513,6 @@ async def process_note_text(message: types.Message, state: FSMContext):
             [InlineKeyboardButton(text="Отмена", callback_data="back_to_main")]
         ])
     )
-    await message.answer()
 
 @router.message(AddNoteStates.waiting_for_type)
 async def process_note_text(message: types.Message, state: FSMContext):
@@ -456,8 +559,8 @@ async def process_calendar_selection(callback: types.CallbackQuery, state: FSMCo
 
     if data[0] == "select":
         # Выбран день
-        year, month, day = int(data[2]), int(data[3]), int(data[4])
-        selected_date = date(year, month, day)
+        year, month, day = int(data[4]), int(data[3]), int(data[2])
+        selected_date = date(day, month, year)
         user_data = await state.get_data()
 
         # Сохраняем заметку в БД
@@ -465,7 +568,7 @@ async def process_calendar_selection(callback: types.CallbackQuery, state: FSMCo
             DATABASE_NAME,
             callback.from_user.id,
             user_data["note_text"], user_data["note_type"],
-            selected_date.strftime("%Y-%m-%d"),
+            selected_date.strftime("%d-%m-%Y"),
             f"{user_data['selected_hour']:02d}:{user_data['selected_minute']:02d}"
         )
 
@@ -477,7 +580,7 @@ async def process_calendar_selection(callback: types.CallbackQuery, state: FSMCo
         ])
 
         await callback.message.edit_text(
-            f"Заметка добавлена:\n<b>{selected_date.strftime('%Y-%m-%d')} {user_data['selected_hour']:02d}:{user_data['selected_minute']:02d}</b>\n{user_data['note_text']}",
+            f"Заметка добавлена:\n<b>{selected_date.strftime("%d-%m-%Y")} {user_data['selected_hour']:02d}:{user_data['selected_minute']:02d}</b>\n\"{user_data['note_text']}\" в категории \"{user_data['note_type']}\"",
             reply_markup=keyboard,
             parse_mode="HTML"
         )
@@ -496,7 +599,7 @@ async def process_calendar_selection(callback: types.CallbackQuery, state: FSMCo
             DATABASE_NAME,
             callback.from_user.id,
             user_data["note_text"], user_data["note_type"],
-            selected_date.strftime("%Y-%m-%d"),
+            selected_date.strftime("%d-%m-%Y"),
             f"{user_data['selected_hour']:02d}:{user_data['selected_minute']:02d}"
         )
 
@@ -508,7 +611,7 @@ async def process_calendar_selection(callback: types.CallbackQuery, state: FSMCo
         ])
 
         await callback.message.edit_text(
-            f"Заметка добавлена:\n<b>{selected_date.strftime('%Y-%m-%d')} {user_data['selected_hour']:02d}:{user_data['selected_minute']:02d}</b>\n{user_data['note_text']}, {user_data['note_type']}",
+            f"Заметка добавлена:\n<b>{selected_date.strftime("%d-%m-%Y")} {user_data['selected_hour']:02d}:{user_data['selected_minute']:02d}</b>\n{user_data['note_text']}, {user_data['note_type']}",
             reply_markup=keyboard,
             parse_mode="HTML"
         )
@@ -536,6 +639,99 @@ async def process_calendar_selection(callback: types.CallbackQuery, state: FSMCo
 
 
 
+
+
+""""поиск по дате"""
+
+@router.callback_query(F.data == "show_by_date")
+async def ask_date_for_notes_handler(callback: types.CallbackQuery, state: FSMContext):
+    """Запрашивает дату для поиска заметок через календарь"""
+    await state.set_state(SearchStates.waiting_for_search_date)
+    await callback.message.edit_text(
+        "Выберите дату для поиска заметок:",
+        reply_markup=generate_calendar()
+    )
+    await callback.answer()
+
+@router.callback_query(F.data.startswith(("prev_month_", "next_month_", "select_day_", "today_", "tomorrow_")),
+                       SearchStates.waiting_for_search_date)
+async def process_search_date_selection(callback: types.CallbackQuery, state: FSMContext):
+    """Обрабатывает выбор даты из календаря для поиска"""
+    data = callback.data.split("_")
+
+    if data[0] == "select":
+        # Выбран день
+        year, month, day = int(data[2]), int(data[3]), int(data[4])
+        search_date = date(year, month, day)
+        await handle_date_search(callback, search_date)
+        await state.clear()
+    elif data[0] == "today":
+        # Сегодня
+        search_date = datetime.now().date()
+        await handle_date_search(callback, search_date)
+        await state.clear()
+    elif data[0] == "tomorrow":
+        # Завтра
+        search_date = datetime.now().date() + timedelta(days=1)
+        await handle_date_search(callback, search_date)
+        await state.clear()
+    elif data[0] == "prev":
+        # Переход к предыдущему месяцу
+        year, month = int(data[2]), int(data[3])
+        month -= 1
+        if month < 1:
+            month = 12
+            year -= 1
+        await callback.message.edit_reply_markup(reply_markup=generate_calendar(year, month))
+    elif data[0] == "next":
+        # Переход к следующему месяцу
+        year, month = int(data[2]), int(data[3])
+        month += 1
+        if month > 12:
+            month = 1
+            year += 1
+        await callback.message.edit_reply_markup(reply_markup=generate_calendar(year, month))
+
+    await callback.answer()
+
+async def  handle_date_search(callback: types.CallbackQuery, search_date: date):
+    """Показывает заметки для выбранной даты"""
+    user_id = callback.from_user.id
+    notes = await get_notes_by_date(DATABASE_NAME, user_id, search_date.strftime("%d-%m-%Y"))
+
+    if not notes:
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Попробовать другую дату", callback_data="show_by_date")],
+            [InlineKeyboardButton(text="В главное меню", callback_data="back_to_main")]
+        ])
+        await callback.message.edit_text(
+            f"На {search_date.strftime('%d-%m-%Y')} заметок не найдено",
+            reply_markup=keyboard
+        )
+        return
+
+    message_text = f"Заметки на {search_date.strftime('%d-%m-%Y')}:\n\n"
+    for note in notes:
+        message_text += f"⏰ {note['note_time']} - {note['note_text']}\n"
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Посмотреть все заметки", callback_data="list_notes")],
+        [InlineKeyboardButton(text="Искать другую дату", callback_data="show_by_date")],
+        [InlineKeyboardButton(text="В главное меню", callback_data="back_to_main")]
+    ])
+
+    await callback.message.edit_text(
+        message_text,
+        reply_markup=keyboard
+    )
+
+
+
+
+
+
+
+
 """напоминания"""
 
 async def check_reminders(bot: Bot):
@@ -554,7 +750,7 @@ async def check_reminders(bot: Bot):
             note_time_str = note['note_time']
 
             try:
-                note_datetime = datetime.strptime(f"{note_date_str} {note_time_str}", "%Y-%m-%d %H:%M")
+                note_datetime = datetime.strptime(f"{note_date_str} {note_time_str}", "%d-%m-%Y %H:%M")
             except ValueError:
                 logging.error(f"Неверный формат даты/времени для заметки ID {note_id}: {note_date_str} {note_time_str}")
                 continue # Пропускаем эту заметку
